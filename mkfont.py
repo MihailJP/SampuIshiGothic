fd5c01cff91ec9b8a9182b741e7be01c176b7bf5
MihailJP <mihailjp@gmail.com>
2025-04-11 22:21:55 +0900

    Add Hiragana to Hentaigana conversion table

--- mkfont.py
+++ mkfont.py
@@ -18,6 +18,78 @@ blockElements = {0x2429} \
 	| set(range(0x1cc21, 0x1cc30)) \
 	| set(range(0x1fb00, 0x1fbf0))
 
+hiraganaToHentaigana = {
+	# あ
+	'uni3042': ('u1B002', 'u1B003', 'u1B004', 'u1B005',),
+	'uni3044': ('u1B006', 'u1B007', 'u1B008', 'u1B009',),
+	'uni3046': ('u1B00A', 'u1B00B', 'u1B00C', 'u1B00D', 'u1B00E',),
+	'uni3048': ('u1B001', 'u1B00F', 'u1B010', 'u1B011', 'u1B012', 'u1B013',),
+	'uni304A': ('u1B014', 'u1B015', 'u1B016',),
+
+	# か
+	'uni304B': ('u1B017', 'u1B018', 'u1B019', 'u1B01A', 'u1B01B', 'u1B01C', 'u1B01D', 'u1B01E', 'u1B01F', 'u1B020', 'u1B021', 'u1B022',),
+	'uni304D': ('u1B023', 'u1B024', 'u1B025', 'u1B026', 'u1B027', 'u1B028', 'u1B029', 'u1B02A', 'u1B03B',),
+	'uni304F': ('u1B02B', 'u1B02C', 'u1B02D', 'u1B02E', 'u1B02F', 'u1B030', 'u1B031',),
+	'uni3051': ('u1B032', 'u1B033', 'u1B034', 'u1B035', 'u1B036', 'u1B037', 'u1B022',),
+	'uni3053': ('u1B038', 'u1B039', 'u1B03A', 'u1B03B', 'u1B098',),
+
+	# さ
+	'uni3055': ('u1B03C', 'u1B03D', 'u1B03E', 'u1B03F', 'u1B040', 'u1B041', 'u1B042', 'u1B043',),
+	'uni3057': ('u1B044', 'u1B045', 'u1B046', 'u1B047', 'u1B048', 'u1B049',),
+	'uni3059': ('u1B04A', 'u1B04B', 'u1B04C', 'u1B04D', 'u1B04E', 'u1B04F', 'u1B050', 'u1B051',),
+	'uni305B': ('u1B052', 'u1B053', 'u1B054', 'u1B055', 'u1B056',),
+	'uni305D': ('u1B057', 'u1B058', 'u1B059', 'u1B05A', 'u1B05B', 'u1B05C', 'u1B05D',),
+
+	# た
+	'uni305F': ('u1B05E', 'u1B05F', 'u1B060', 'u1B061',),
+	'uni3061': ('u1B062', 'u1B063', 'u1B064', 'u1B065', 'u1B066', 'u1B067', 'u1B068', ),
+	'uni3064': ('u1B069', 'u1B06A', 'u1B06B', 'u1B06C', 'u1B06D',),
+	'uni3066': ('u1B06E', 'u1B06F', 'u1B070', 'u1B071', 'u1B072', 'u1B073', 'u1B074', 'u1B075', 'u1B076', 'u1B08E',),
+	'uni3068': ('u1B077', 'u1B078', 'u1B079', 'u1B07A', 'u1B07B', 'u1B07C', 'u1B07D', 'u1B06D',),
+
+	# な
+	'uni306A': ('u1B07E', 'u1B07F', 'u1B080', 'u1B081', 'u1B082', 'u1B083', 'u1B084', 'u1B085', 'u1B086',),
+	'uni306B': ('u1B087', 'u1B088', 'u1B089', 'u1B08A', 'u1B08B', 'u1B08C', 'u1B08D', 'u1B08E',),
+	'uni306C': ('u1B08F', 'u1B090', 'u1B091',),
+	'uni306D': ('u1B092', 'u1B093', 'u1B094', 'u1B095', 'u1B096', 'u1B097', 'u1B098',),
+	'uni306E': ('u1B099', 'u1B09A', 'u1B09B', 'u1B09C', 'u1B09D',),
+
+	# は
+	'uni306F': ('u1B09E', 'u1B09F', 'u1B0A0', 'u1B0A1', 'u1B0A2', 'u1B0A3', 'u1B0A4', 'u1B0A5', 'u1B0A6', 'u1B0A7', 'u1B0A8',),
+	'uni3072': ('u1B0A9', 'u1B0AA', 'u1B0AB', 'u1B0AC', 'u1B0AD', 'u1B0AE', 'u1B0AF',),
+	'uni3075': ('u1B0B0', 'u1B0B1', 'u1B0B2',),
+	'uni3078': ('u1B0B3', 'u1B0B4', 'u1B0B5', 'u1B0B6', 'u1B0B7', 'u1B0B8', 'u1B0B9',),
+	'uni307B': ('u1B0BA', 'u1B0BB', 'u1B0BC', 'u1B0BD', 'u1B0BE', 'u1B0BF', 'u1B0C0', 'u1B0C1',),
+
+	# ま
+	'uni307E': ('u1B0C2', 'u1B0C3', 'u1B0C4', 'u1B0C5', 'u1B0C6', 'u1B0C7', 'u1B0C8', 'u1B0D6',),
+	'uni307F': ('u1B0C9', 'u1B0CA', 'u1B0CB', 'u1B0CC', 'u1B0CD', 'u1B0CE', 'u1B0CF',),
+	'uni3080': ('u1B0D0', 'u1B0D1', 'u1B0D2', 'u1B0D3', 'u1B11D', 'u1B11E',),
+	'uni3081': ('u1B0D4', 'u1B0D5', 'u1B0D6',),
+	'uni3082': ('u1B0D7', 'u1B0D8', 'u1B0D9', 'u1B0DA', 'u1B0DB', 'u1B0DC', 'u1B11D', 'u1B11E',),
+
+	# や
+	'uni3084': ('u1B0DD', 'u1B0DE', 'u1B0DF', 'u1B0E0', 'u1B0E1', 'u1B0E2',),
+	'uni3086': ('u1B0E3', 'u1B0E4', 'u1B0E5', 'u1B0E6',),
+	'uni3088': ('u1B0E7', 'u1B0E8', 'u1B0E9', 'u1B0EA', 'u1B0EB', 'u1B0EC', 'u1B0E2',),
+
+	# ら
+	'uni3089': ('u1B0ED', 'u1B0EE', 'u1B0EF', 'u1B0F0', 'u1B07D',),
+	'uni308A': ('u1B0F1', 'u1B0F2', 'u1B0F3', 'u1B0F4', 'u1B0F5', 'u1B0F6', 'u1B0F7',),
+	'uni308B': ('u1B0F8', 'u1B0F9', 'u1B0FA', 'u1B0FB', 'u1B0FC', 'u1B0FD',),
+	'uni308C': ('u1B0FE', 'u1B0FF', 'u1B100', 'u1B101',),
+	'uni308D': ('u1B102', 'u1B103', 'u1B104', 'u1B105', 'u1B106', 'u1B107',),
+
+	# わ
+	'uni308F': ('u1B108', 'u1B109', 'u1B10A', 'u1B10B', 'u1B10C',),
+	'uni3090': ('u1B10D', 'u1B10E', 'u1B10F', 'u1B110', 'u1B111',),
+	'uni3091': ('u1B112', 'u1B113', 'u1B114', 'u1B115',),
+	'uni3092': ('u1B116', 'u1B117', 'u1B118', 'u1B119', 'u1B11A', 'u1B11B', 'u1B11C', 'u1B005',),
+
+	# ん
+	'uni3093': ('u1B11D', 'u1B11E',),
+}
+
 iwaFont = bool(re.search("SampuIwaGothic", argv[1]))
 
 def isToBeConvertedToFullwidth(font, glyph):
@@ -227,7 +299,7 @@ genseki.addLookup("Stylistic Kana alternates", 'gsub_single', None, (('salt', ((
 genseki.addLookupSubtable("Stylistic Kana alternates", "Stylistic Hiragana alternates")
 genseki.addLookup("Historical Kana", 'gsub_alternate', None, (('hist', (('kana', ('dflt',)),)),))
 genseki.addLookupSubtable("Historical Kana", "Historical Katakana")
-#genseki.addLookupSubtable("Historical Kana", "Hiragana to Hentaigana")
+genseki.addLookupSubtable("Historical Kana", "Hiragana to Hentaigana")
 
 # グリフの変更
 patchFont = fontforge.open(argv[4])
@@ -257,6 +329,50 @@ for glyph in patchFont:
 	genseki.selection.select(glyph)
 	genseki.paste()
 patchFont.close(); patchFont = None
+
+# 変体仮名検索用
+addDakuten = lambda x: x + '_uni3099.ccmp'
+addHandakuten = lambda x: x + '_uni309A.ccmp'
+checkExistence = lambda x: x in genseki
+hiraganaToHentaigana.update({
+	# が
+	'uni304C': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni304B']))),
+	'uni304E': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni304D']))),
+	'uni3050': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni304F']))),
+	'uni3052': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3051']))),
+	'uni3054': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3053']))),
+
+	# ざ
+	'uni3056': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3055']))),
+	'uni3058': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3057']))),
+	'uni305A': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3059']))),
+	'uni305C': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni305B']))),
+	'uni305E': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni305D']))),
+
+	# だ
+	'uni3060': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni305F']))),
+	'uni3062': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3061']))),
+	'uni3065': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3064']))),
+	'uni3067': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3066']))),
+	'uni3069': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3068']))),
+
+	# ば
+	'uni3070': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni306F']))),
+	'uni3073': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3072']))),
+	'uni3076': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3075']))),
+	'uni3079': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni3078']))),
+	'uni307C': tuple(filter(checkExistence, map(addDakuten, hiraganaToHentaigana['uni307B']))),
+
+	# ぱ
+	'uni3071': tuple(filter(checkExistence, map(addHandakuten, hiraganaToHentaigana['uni306F']))),
+	'uni3074': tuple(filter(checkExistence, map(addHandakuten, hiraganaToHentaigana['uni3072']))),
+	'uni3077': tuple(filter(checkExistence, map(addHandakuten, hiraganaToHentaigana['uni3075']))),
+	'uni307A': tuple(filter(checkExistence, map(addHandakuten, hiraganaToHentaigana['uni3078']))),
+	'uni307D': tuple(filter(checkExistence, map(addHandakuten, hiraganaToHentaigana['uni307B']))),
+})
+for glyph in hiraganaToHentaigana.items():
+	if len(glyph[1]) > 0:
+		genseki[glyph[0]].addPosSub("Hiragana to Hentaigana", glyph[1])
 
 # ギリシア・ロシア・一部の記号
 for glyph in genseki:
